3b68b20372e55d69476732f354a0664a27c6aeea
Rebin Silva <rebinsilva@gmail.com>
2024-03-15 12:22:41 -0700

    Locally working commit

---
+++ main.cpp
@@ -0,0 +1,128 @@
+#include <algorithm>
+#include <vector>
+#include <stdio.h>
+#include <stdlib.h>
+#include <cstring>
+#include <mpi.h>
+#include <boost/mpi/datatype.hpp>
+
+#define BAND_SIZE 2
+#define M 6  // Size of the matrix_B
+#define K 8  // Size of the matrix_B
+#define N 6  // Size of the matrix_B
+
+template <typename T>
+struct genMatrix {
+    unsigned int nRows;
+    unsigned int nCols;
+    T counter = 0;
+
+    genMatrix(unsigned int nRows, unsigned int nCols, T init=0): nRows(nRows), nCols(nCols), counter(init) {}
+
+    T operator()()
+    {
+	return (++counter);
+    }
+};
+
+
+template <typename T>
+void matrixMult()
+{
+    int rank, size;
+    T* column = (T*)calloc(BAND_SIZE * K, sizeof(T)); // Buffer to receive the column
+    T* row = (T*)calloc(BAND_SIZE * K, sizeof(T)); // Buffer to receive the column
+							 //
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    std::vector<T> matrix_A(M*K);
+    std::vector<T> matrix_B(K*N);
+
+    // std::generate(matrix_A.begin(), matrix_A.end(), [n = 0] () mutable { return n++; });
+    std::generate(matrix_A.begin(), matrix_A.end(), genMatrix<T>(M, K));
+    std::generate(matrix_B.begin(), matrix_B.end(), genMatrix<T>(K, N, 100));
+
+    // Process 0 prints the original matrix_B
+    if (rank == 0) {
+	std::cout<<"Original matrix_A:"<<std::endl;
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < K; j++)
+                std::cout<<matrix_A[i*K + j] << "\t";
+            std::cout<<std::endl;
+        }
+	std::cout<<std::endl;
+
+	std::cout<<"Original matrix_B:"<<std::endl;;
+        for (int i = 0; i < K; i++) {
+            for (int j = 0; j < N; j++)
+                std::cout<<matrix_B[i*N + j] << "\t";
+            std::cout<<std::endl;
+        }
+    }
+    std::cout<<std::endl;
+
+    // Define the datatype for a column
+    MPI_Datatype col, coltype;
+    MPI_Type_vector(K, BAND_SIZE, N, boost::mpi::get_mpi_datatype<T>(), &col);
+    MPI_Type_commit(&col);
+    MPI_Type_create_resized(col, 0, BAND_SIZE*sizeof(T), &coltype);
+    MPI_Type_commit(&coltype);
+
+    for(int c=0; (c+rank)*BAND_SIZE < N; c+=size)
+    {
+	// Scatter the columns of the matrix_B
+	MPI_Scatter(matrix_B.data() + (c+rank)*BAND_SIZE, 1, coltype, column, BAND_SIZE*K, boost::mpi::get_mpi_datatype<T>(), 0, MPI_COMM_WORLD);
+
+	for(int r=0; r*BAND_SIZE < M; r++)
+	{
+	    if (rank == 0)
+	    {
+		std::memcpy(row, matrix_A.data() + r * K * BAND_SIZE, BAND_SIZE * K * sizeof(T));
+	    }
+
+	    // Broadcast the rows of the matrix_A
+	    MPI_Bcast(row, BAND_SIZE * K, boost::mpi::get_mpi_datatype<T>(), 0, MPI_COMM_WORLD);
+		
+
+	    // Each process prints the received column
+	    std::cout<<"Process "<<rank<<" received row band: ";
+	     for (int i = 0; i < BAND_SIZE * K; i++)
+		std::cout<<row[i]<<" ";
+	    std::cout<<std::endl;
+
+	    // Each process prints the received column
+	    std::cout<<"Process "<<rank<<" received column band: ";
+	    for (int i = 0; i < BAND_SIZE * K; i++)
+		std::cout<<column[i]<<" ";
+	    std::cout<<std::endl;
+	}
+    }
+
+
+    MPI_Type_free(&coltype);
+    MPI_Type_free(&col);
+
+    free(column);
+    free(row);
+}
+
+int main(int argc, char *argv[]) {
+    int rank, size;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    if (BAND_SIZE*size > K || M % BAND_SIZE != 0 || N % BAND_SIZE != 0 || K%size != 0) {
+        if (rank == 0)
+            printf("Prereq issue.\n");
+        MPI_Finalize();
+        return 1;
+    }
+
+    matrixMult<double>();
+
+    MPI_Finalize();
+    return 0;
+}
+
